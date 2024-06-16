7fc34a4e1cf8f68d5fddc3a3ae75c8393ddf3a65
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-06-16 06:26:26 -0500

    Create folder Python 3 empieza desde cero Create Conceptos basicos iniciales -> Python 3 empieza desde cero

---
+++ Python/Udemy Python 3 empieza desde cero/Conceptos basicos iniciales.py
@@ -0,0 +1,14 @@
+#conceptos basicos iniciales
+
+print("Hola mundo")
+
+#las variables no se declaran, solo se coloca el nombre
+name = "nombre"
+print(name)
+
+#con asignarles un valor diferente cambian de tipo
+
+name = 55
+
+name = 55-1
+
