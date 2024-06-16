e1a6d7cc094730d5f4b2e183641a32bd6291e141
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-06-16 06:29:43 -0500

    Modified file Conceptos basicos iniciales

--- Python/Udemy Python 3 empieza desde cero/Conceptos basicos iniciales.py
+++ Python/Udemy Python 3 empieza desde cero/Conceptos basicos iniciales.py
@@ -12,3 +12,47 @@ name = 55
 
 name = 55-1
 
+print(name)
+
+#variables de tipo numerico
+
+num_1 = 20
+num_2 = 5
+
+print(num_1)
+print(num_2)
+
+#no existen declaraciones de tipo de variable numerica
+
+num_2 = num_2/num_1
+print(num_2) 
+
+#imprime decimal sin necesidad de declararlo
+
+#cadenas de caracteres
+
+cadena_1 = "primer"
+cadena_2 = "ejemplo"
+
+#formas de impresion
+
+#forma 1
+
+print(cadena_1, cadena_2)
+
+#forma 2
+
+print("este es:", cadena_1, cadena_2)
+
+#forma 3
+
+print(f'este es: {cadena_1} {cadena_2}')
+
+#imprimir resultados de operaciones
+
+print(f'tengo {10+10} años')
+
+#imprimir varias veces la misma variable
+
+print(cadena_1*5)
+
